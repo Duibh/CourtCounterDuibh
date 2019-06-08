4a75db4ed304d4edc13413f4705f17c0113361b7
Duibh <32749737+Duibh@users.noreply.github.com>
2019-06-08 13:11:28 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -16,9 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * Displays the given score for Team A.
-     */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
